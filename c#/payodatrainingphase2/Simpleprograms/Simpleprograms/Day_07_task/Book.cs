7a3774dccec03675a43a5e473bfef413e50a6dbb
Harshavarthan01 <141032776+Harshavarthan01@users.noreply.github.com>
2023-08-07 22:12:27 +0530

    Add files via upload

---
+++ c#/payodatrainingphase2/Simpleprograms/Simpleprograms/Day_07_task/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Book : ILendable
+    {
+        private readonly int book_id;
+        private string? title;
+        private string? author;
+        private bool isAvailable;
+
+        public Book(int book_id, string? title, string? author, bool isAvailable) 
+        {
+            this.book_id = book_id;
+            this.title = title;
+            this.author = author;
+            this.isAvailable = isAvailable;
+        }
+
+        public int BookId => book_id;
+
+        public string? Title { get => title; set => title = value; }
+        public string? Author { get => author; set => author = value; }
+        public bool IsAvailable { get => isAvailable; set => isAvailable = value; }
+
+        public void DisplayDetails() {
+            Console.WriteLine( BookId + "  " + Title + "  " + Author + "  " + IsAvailable);
+        }
+
+        public  static void LendItem(string? title) {
+            int count = 0;
+            foreach (Book i in Library.bookslist)
+            {
+                if (i.Title.ToLower().Equals(title.ToLower()))
+                {
+                    i.IsAvailable = false;
+                    Console.WriteLine("Borrowed");
+                    count++;
+                }
+            }
+            if (count == 0) { Console.WriteLine("Book not Available"); }
+        }
+    }
+}
---
+++ c#/payodatrainingphase2/Simpleprograms/Simpleprograms/Day_07_task/Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Library 
+    {
+        public static ArrayList bookslist = new ArrayList();
+       
+        public void BorrowBook(string title)
+        {
+            Book.LendItem(title);
+        }
+        public void ReturnBook(string title)
+        {
+            foreach (Book i in bookslist)
+            {
+                if (i.Title.Equals(title))
+                {
+                    i.IsAvailable = true;
+                    Console.WriteLine("Returned");
+                }
+            }
+
+        }
+        public void DisplayBookDetails()
+        {
+            foreach (Book i in bookslist)
+            {
+                Console.WriteLine("Title :" + i.Title + " Author :" + i.Author + " Availablity " + i.IsAvailable);
+            }
+        }
+    }
+}
