630f8262cc3595e28785b7a8bc905016df8043b6
Harshavarthan01 <141032776+Harshavarthan01@users.noreply.github.com>
2023-08-12 20:20:48 +0530

    Add files via upload

---
+++ C#/Day_13 task/StudentManagement/Day_13 task/StudentManagement/StudentDetails/Services/Interfaces/IStudent.cs
@@ -0,0 +1,18 @@
+﻿using StudentDetails.Models;
+
+namespace StudentDetails.Services.Interfaces
+{
+    public interface IStudent
+    {
+        Task<List<Student>> GetAllStudents();
+
+        public Task<Student> GetStudentById(int id);
+
+        public Task<List<Student>> AddStudent(Student student);
+
+        public Task<Student> UpdateStudent(int rollno, Student student);
+
+        public Task<List<Student>> DeleteStudent(int id);
+
+    }
+}
