9094be9413c1591a967b6ef1928a5828cb3bbfa6
Harshavarthan01 <141032776+Harshavarthan01@users.noreply.github.com>
2023-08-13 15:02:56 +0530

    Add files via upload

---
+++ C#/Day_13 task/StudentManagement/StudentDBDetails/Context/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentDBDetails.Models;
+
+namespace StudentDBDetails.Context
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        
+    }
+}
